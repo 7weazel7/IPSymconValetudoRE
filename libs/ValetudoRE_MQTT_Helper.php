5e9c0ca5c61fd679fb42d0897229c8dd9e69b18d
Florian Uetz <uetzfl@MacBookAir-Florian.local>
2020-09-30 17:07:42 +0200

    s

--- libs/ValetudoRE_MQTT_Helper.php
+++ libs/ValetudoRE_MQTT_Helper.php
@@ -1,9 +1,19 @@
 <?php /** @noinspection AutoloadingIssuesInspection */
 
 declare(strict_types=1);
-define('MQTT_GROUP_TOPIC', 'ebusd');
 
 trait ValetudoREMQTTHelper
 {
+    protected function MsgBox(string $Message): void
+    {
+        $this->UpdateFormField('MsgText', 'caption', $Message);
 
+        $this->UpdateFormField('MsgBox', 'visible', true);
+    }
+    protected function GetParent($instanceID)
+    {
+        $instance = IPS_GetInstance($instanceID);
+
+        return ($instance['ConnectionID'] > 0) ? $instance['ConnectionID'] : 0;
+    }
 }
