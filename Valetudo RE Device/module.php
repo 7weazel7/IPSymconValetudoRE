3c0d85cd8308bc6a3032ecd346848bb6eb62b311
Uetz <uetz@goldhofer.intern>
2020-10-01 11:46:58 +0200

    s

--- Valetudo RE Device/module.php
+++ Valetudo RE Device/module.php
@@ -141,7 +141,7 @@ require_once __DIR__ . '/../libs/ValetudoRE_MQTT_Helper.php';
 				$mqttConfig
 			);
 			$result = $this->readURL($url);
-			$this->Logger_Dbg(__FUNCTION__, sprintf('Result readURL: %s', echo($result)));
+			$this->Logger_Dbg(__FUNCTION__, sprintf('Result readURL: %s', json_encode($result)));
 
 			if ($this->GetStatus() !== IS_ACTIVE) {
 				$this->SetStatus(IS_ACTIVE);
