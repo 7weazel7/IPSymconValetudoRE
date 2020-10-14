c02f85734d8f7fb8d97a44f54a8831427559b33c
Uetz <uetz@goldhofer.intern>
2020-10-14 08:42:17 +0200

    s

--- Valetudo RE Device/module.php
+++ Valetudo RE Device/module.php
@@ -212,7 +212,10 @@ require_once __DIR__ . '/../libs/ValetudoRE_MQTT_Helper.php';
 
 			switch ($Buffer->Topic) {
 				case $mqttTopic . '/destinations':
-					$this->Logger_Dbg('MQTT Topic - destinations', sprintf('Payload rooms: %s', $Payload["rooms"]));
+					foreach($Payload->rooms as $room) {
+							$this->Logger_Dbg('MQTT Topic - destinations', sprintf('Payload room: %s', $room));
+						}
+					}
 					break;
 				default:
 					//$this->Logger_Dbg('MQTT Topic - destinations', sprintf('Payload: %s', $Payload));
